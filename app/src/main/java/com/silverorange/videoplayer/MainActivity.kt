05920697a86933b200e16398f6f004dfc7e25af5
Raul Vieira <raulguil@gmail.com>
2022-10-06 20:05:37 -0400

    Add ExoPlayer functionalities

--- app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -1,15 +1,63 @@
 package com.silverorange.videoplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
+import io.noties.markwon.Markwon
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var player: ExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        player = ExoPlayer.Builder(this).build()
+        player.addListener(object : Player.Listener {
+            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                super.onMediaItemTransition(mediaItem, reason)
+                mediaItem?.mediaId?.let { setVideoDetails(it) }
+            }
+        })
+        binding.videoPlayer.player = player
+
+        viewModel.videoList.observe(this) { videoList ->
+            for ((position, videoItem) in videoList.withIndex()) {
+                player.addMediaItem(
+                    MediaItem.Builder()
+                        .setUri(videoItem.hlsURL)
+                        .setMediaId(position.toString())
+                        .build()
+                )
+            }
+            player.prepare()
+            setVideoDetails("0")
+        }
+    }
+
+    fun setVideoDetails(position: String) {
+        if (viewModel.videoList.value.isNullOrEmpty()) return
+
+        val videoItem = viewModel.videoList.value!![position.toInt()]
+        val markwon = Markwon.builder(this)
+            .build()
+
+        with(binding) {
+            textviewTitle.text = videoItem.title
+            textviewAuthor.text = videoItem.author.name
+            markwon.setMarkdown(textviewDescription, videoItem.description)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
     }
 }
